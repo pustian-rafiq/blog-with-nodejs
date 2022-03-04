8f9e79c4cb21811e74f67006f5455605ceccbc76
pustian-rafiq <rafiqul.pust.cse@gmail.com>
2022-03-04 16:24:13 +0600

    create signup page

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
 
+// Setup View Engine
+app.set('view engine','ejs')
+app.set('views','views')
+
+//Middleware Array
+const middleware =[
+    morgan('dev'),
+    express.static('public'),
+    express.urlencoded({extended: true}),
+    express.json()
+]
+
+// Use middleware
+app.use(middleware)
+
+// Route directory 
 app.get('/', (req, res) => {
+
+    res.render('pages/auth/signup',{title: 'Create a new account'})
+
     res.json({
         message: 'Hello world'
     })
 })
 
-const PORT = process.env.PORT
+// Create server 
+const PORT = process.env.PORT || 8080
 app.listen(PORT,()=> {
     console.log(`Server is running on port ${PORT}`)
 })
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,4 +1,20 @@
 const router = require('express').Router();
 
+const {
+    signupGetController,
+    signupPostController,
+    loginGetController,
+    loginPostController,
+    logoutController
+} = require('../controllers/authController')
+
+router.get('/signup', signupGetController)
+router.get('/signup', signupPostController)
+
+router.get('/login', loginGetController)
+router.get('/login', loginPostController)
+
+router.get('/logout', logoutController)
+
 
 module.exports = router
